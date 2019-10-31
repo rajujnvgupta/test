42cf05f119b67fc903889411ca4a67129c972caa
rajujnvgupta <39230239+rajujnvgupta@users.noreply.github.com>
2019-10-31 12:57:16 +0530

    Update custom_debugger.cpp

--- custom_debugger.cpp
+++ custom_debugger.cpp
@@ -1,6 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
+
 #define imie(...) cout<<" "<<#__VA_ARGS__<<(__VA_ARGS__)<<endl;
+#define loop(n) for(int ii = 0; ii < n; ++ ii)
 #define DEBUG 1
 #if(DEBUG == 1)                        
 #define ASSERT(...)                             \
